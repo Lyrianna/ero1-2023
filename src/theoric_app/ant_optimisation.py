53f48e846da892e64585dfc6a6c1e91877c34a23
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-05-27 02:31:16 +0200

    Tried to implement ant optimization with adj_list

--- src/theoric_app/ant_optimisation.py
+++ src/theoric_app/ant_optimisation.py
@@ -1,9 +1,13 @@
+import random
+import sys
+
 import numpy as np
+from numpy.random import choice as np_choice
 
 
 class AntColony(object):
 
-    def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
+    def __init__(self, adj_list, distances, pheromones, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
         """
         Args:
             distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
@@ -16,9 +20,9 @@ class AntColony(object):
         Example:
             ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)
         """
+        self.adj_list = adj_list
         self.distances = distances
-        self.pheromone = np.ones(self.distances.shape) / len(distances)
-        self.all_inds = range(len(distances))
+        self.pheromone = pheromones
         self.n_ants = n_ants
         self.n_best = n_best
         self.n_iterations = n_iterations
@@ -34,49 +38,76 @@ class AntColony(object):
             self.spread_pheronome(all_paths, self.n_best, shortest_path=shortest_path)
             shortest_path = min(all_paths, key=lambda x: x[1])
             print(shortest_path)
-            if shortest_path[1] < all_time_shortest_path[1]:
+            if shortest_path[1] < all_time_shortest_path[1]:# or all_time_shortest_path[1] == np.inf:
                 all_time_shortest_path = shortest_path
-            self.pheromone = self.pheromone * self.decay
+            # self.pheromone = self.pheromone * self.decay
+            for key in self.pheromone:
+                self.pheromone[key] *= self.decay
         return all_time_shortest_path
 
     def spread_pheronome(self, all_paths, n_best, shortest_path):
         sorted_paths = sorted(all_paths, key=lambda x: x[1])
         for path, dist in sorted_paths[:n_best]:
             for move in path:
-                self.pheromone[move] += 1.0 / self.distances[move]
+                start, end = move[0], move[1]
+                end_index = self.adj_list[start].index(end)
+                self.pheromone[start][end_index] += 1.0 / self.distances[start][end_index]
 
     def gen_path_dist(self, path):
         total_dist = 0
-        for ele in path:
-            total_dist += self.distances[ele]
+        for edge in path:
+            start, end = edge[0], edge[1]
+            total_dist += self.distances[start][self.adj_list[start].index(end)]
+
         return total_dist
 
     def gen_all_paths(self):
         all_paths = []
         for i in range(self.n_ants):
-            path = self.gen_path(0)
+            start_node = next(iter(self.adj_list))
+            path = self.gen_path(start_node)
             all_paths.append((path, self.gen_path_dist(path)))
         return all_paths
 
-    def gen_path(self, start):
+    def gen_path(self, start_node):
         path = []
         visited = set()
-        visited.add(start)
-        prev = start
-        for i in range(len(self.distances) - 1):
-            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
+        visited.add(start_node)
+        prev = start_node
+        for _ in self.adj_list:
+            move = self.pick_move(self.adj_list[prev], self.pheromone[prev], self.distances[prev], visited)
+            # neighbours = self.adj_list[prev]
+            # if not neighbours:
+            #     break
+            # move = neighbours[random.randint(0, len(neighbours) - 1)]
+            # if move in visited:
+            #     move = neighbours[random.randint(0, len(neighbours) - 1)]
+            if not move:
+                break
             path.append((prev, move))
             prev = move
             visited.add(move)
-        path.append((prev, start))  # going back to where we started
         return path
 
-    def pick_move(self, pheromone, dist, visited):
+    # Pick best move from neighbours
+    def pick_move(self, adj, pheromone, dist, visited):
+
         pheromone = np.copy(pheromone)
-        pheromone[list(visited)] = 0
+        for node in visited:
+            try:
+                i = adj.index(node)
+                pheromone[i] = 0
+            except ValueError:
+                continue
 
         row = pheromone ** self.alpha * ((1.0 / dist) ** self.beta)
+        s = row.sum()
+        if s == 0:
+            return None
+        norm_row = row / s
+        if np.isnan(norm_row).any():
+            return None
 
-        norm_row = row / row.sum()
-        move = np_choice(self.all_inds, 1, p=norm_row)[0]
-        return move
+        # Array de len -> self.all_inds <=> range(len(array))
+        move = np_choice(range(len(dist)), 1, p=norm_row)[0]
+        return adj[move]
--- src/theoric_app/theoric_src.py
+++ src/theoric_app/theoric_src.py
@@ -6,33 +6,83 @@ Problème numéro uno : Trouver une solution de parcours opti -> Cours de THEG i
 -> Ant Optimisation
 Problème numéro deuxio : Choisir ce qu'on préfère entre un parcours complet et un parcours rapide :oof:
 """
+import math
 import sys
 
 import networkx as nx
+import ant_optimisation as ao
 import osmnx as ox
+import numpy as np
+
+from ant_optimisation import AntColony
+import ant_colony as ac
 
 
 def getDistanceMatrix(graph):
-    D = nx.linalg.graphmatrix.adjacency_matrix(graph)
+    mat = nx.to_numpy_array(graph, weight='length')
+    mat[mat == 0.] = sys.maxsize
+    return mat
 
-    print(D)
 
-    return D
+def get_adj_list(city_graph):
+    # Initialize tmp adj_list with nodes
+    tmp = {}
+    for node in city_graph.nodes():
+        tmp[node] = ([], [], [])
+
+    # Populate
+    for edge in city_graph.edges(data=True):
+        start, end, data = edge[0], edge[1], edge[2]
+        tmp[start][0].append(end)
+        tmp[start][1].append(data['length'])
+        tmp[start][2].append(1)
+        if not data['oneway']:
+            tmp[end][0].append(start)
+            tmp[end][1].append(data['length'])
+            tmp[end][2].append(1)
+
+    # Remove dead ends
+    # tmp = {key: _ for key, _ in tmp.items() if len(tmp[key][0]) != 0}
+
+    adj_list = {}
+    distances = {}
+    pheromones = {}
+    for key in tmp:
+        adj_list[key] = tmp[key][0]
+        distances[key] = np.array(tmp[key][1])
+        pheromones[key] = np.array(tmp[key][2]) / len(tmp[key][2])
+
+    return adj_list, distances, pheromones
 
 
 def main():
-    # Load graph from dot file
+    # Load graph from osmnx
     city_name = sys.argv[1]
+    print(city_name)
     city = ox.graph_from_place(city_name, network_type='drive')
 
-    D = getDistanceMatrix(city)
+    adj_list, distances, pheromones = get_adj_list(city)
+    # for key in adj_list:
+    #     print(key, '=', adj_list[key], ',', distances[key], ',', pheromones[key])
 
+    ants = AntColony(adj_list, distances, pheromones, 40, 20, 150, 0.95)
+    optimized_path = ants.run()
 
-# ants = ao.AntColony(D, 40, 20, 40, 0.95)
-# optimized_path = ants.run()
+    print("shorted_path: {}".format(optimized_path))
 
-# nodes, edges = ox.gra(city)
-# print(edges)
+    ec = ['r' if (u, v) in optimized_path[0] else 'w' for u, v, k in city.edges(keys=True)]
+    fig, ax = ox.plot_graph(city, node_color='w', node_edgecolor='k', node_size=30,
+                            node_zorder=3, edge_color=ec, edge_linewidth=3)
+
+    # distances = np.array([[np.inf, 2, 2, 5, 7],
+    #                       [2, np.inf, 4, 8, 2],
+    #                       [2, 4, np.inf, 1, 3],
+    #                       [5, 8, 1, np.inf, 2],
+    #                       [7, 2, 3, 2, np.inf]])
+    #
+    # ant_colony = ac.AntColony(distances, 1, 1, 100, 0.95, alpha=1, beta=1)
+    # shortest_path = ant_colony.run()
+    # print("shorted_path: {}".format(shortest_path))
 
 
 if __name__ == '__main__':
