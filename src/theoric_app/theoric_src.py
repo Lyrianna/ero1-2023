474975dc7c0a387e9ffe25f05f1890a34a03e24e
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-05-28 02:59:21 +0200

    Reworked ant colony optimization, beginning to see some results!

--- src/theoric_app/theoric_src.py
+++ src/theoric_app/theoric_src.py
@@ -6,83 +6,58 @@ Problème numéro uno : Trouver une solution de parcours opti -> Cours de THEG i
 -> Ant Optimisation
 Problème numéro deuxio : Choisir ce qu'on préfère entre un parcours complet et un parcours rapide :oof:
 """
-import math
+import os
 import sys
+import time
+import calendar
 
 import networkx as nx
-import ant_optimisation as ao
 import osmnx as ox
-import numpy as np
+from city_graph import CityGraph
 
 from ant_optimisation import AntColony
-import ant_colony as ac
 
-
-def getDistanceMatrix(graph):
-    mat = nx.to_numpy_array(graph, weight='length')
-    mat[mat == 0.] = sys.maxsize
-    return mat
-
-
-def get_adj_list(city_graph):
-    # Initialize tmp adj_list with nodes
-    tmp = {}
-    for node in city_graph.nodes():
-        tmp[node] = ([], [], [])
-
-    # Populate
-    for edge in city_graph.edges(data=True):
-        start, end, data = edge[0], edge[1], edge[2]
-        tmp[start][0].append(end)
-        tmp[start][1].append(data['length'])
-        tmp[start][2].append(1)
-        if not data['oneway']:
-            tmp[end][0].append(start)
-            tmp[end][1].append(data['length'])
-            tmp[end][2].append(1)
-
-    # Remove dead ends
-    # tmp = {key: _ for key, _ in tmp.items() if len(tmp[key][0]) != 0}
-
-    adj_list = {}
-    distances = {}
-    pheromones = {}
-    for key in tmp:
-        adj_list[key] = tmp[key][0]
-        distances[key] = np.array(tmp[key][1])
-        pheromones[key] = np.array(tmp[key][2]) / len(tmp[key][2])
-
-    return adj_list, distances, pheromones
+output_dir = 'output_files'
+ox.config(use_cache=True, log_console=True)
 
 
 def main():
     # Load graph from osmnx
     city_name = sys.argv[1]
     print(city_name)
-    city = ox.graph_from_place(city_name, network_type='drive')
+    osmnx_graph = ox.graph_from_place(city_name, network_type='drive')
+    osmnx_graph = nx.convert_node_labels_to_integers(osmnx_graph)
 
-    adj_list, distances, pheromones = get_adj_list(city)
-    # for key in adj_list:
-    #     print(key, '=', adj_list[key], ',', distances[key], ',', pheromones[key])
+    city_graph = CityGraph(osmnx_graph)
 
-    ants = AntColony(adj_list, distances, pheromones, 40, 20, 150, 0.95)
-    optimized_path = ants.run()
+    ants = AntColony(city_graph, 1, 20, 1, 0.95, alpha=1.5, beta=0.5)
+    path = ants.run()
 
-    print("shorted_path: {}".format(optimized_path))
+    print(path)
 
-    ec = ['r' if (u, v) in optimized_path[0] else 'w' for u, v, k in city.edges(keys=True)]
-    fig, ax = ox.plot_graph(city, node_color='w', node_edgecolor='k', node_size=30,
-                            node_zorder=3, edge_color=ec, edge_linewidth=3)
+    id_path = path.to_id_path()
+    pair_id_path = path.to_pair_id_path()
 
-    # distances = np.array([[np.inf, 2, 2, 5, 7],
-    #                       [2, np.inf, 4, 8, 2],
-    #                       [2, 4, np.inf, 1, 3],
-    #                       [5, 8, 1, np.inf, 2],
-    #                       [7, 2, 3, 2, np.inf]])
+    print(id_path)
+    print(pair_id_path)
+
+    # output_folder = f'{output_dir}/{city_name}-{calendar.timegm(time.gmtime())}'
+    # os.mkdir(output_folder)
     #
-    # ant_colony = ac.AntColony(distances, 1, 1, 100, 0.95, alpha=1, beta=1)
-    # shortest_path = ant_colony.run()
-    # print("shorted_path: {}".format(shortest_path))
+    # for i in range(len(pair_id_path)):
+    #     ec = ['r' if (u, v) in pair_id_path[:i] else 'w' for u, v, k in osmnx_graph.edges(keys=True)]
+    #     print(ec)
+    #     ox.plot_graph(osmnx_graph, node_color='w', node_edgecolor='k', node_size=10, node_zorder=3, edge_color=ec,
+    #                   edge_linewidth=1, show=False, save=True,
+    #                   filepath=f'{output_folder}/{i}.png')
+
+    # nc = ['b' if node_id in id_path else 'w' for node_id in osmnx_graph.nodes()]
+    ec = ['r' if (u, v) in pair_id_path else 'w' for u, v, k in osmnx_graph.edges(keys=True)]
+    print(ec)
+    # fig, ax = ox.plot_graph(osmnx_graph, node_color='w', node_edgecolor='k', node_size=10, node_zorder=3,
+    # edge_color=ec, edge_linewidth=1, save=True, filepath=f'{output_dir}/output.png')
+    fig, ax = ox.plot_graph_route(osmnx_graph, route=id_path, route_color='r', route_linewidth=3, orig_dest_size=30,
+                                  node_size=2, save=True, filepath=f'{output_dir}/output.png')
 
 
 if __name__ == '__main__':
