5c0db7f6fe1e8dddebea716b06d0d17de33564e0
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-05-31 23:49:56 +0200

    Added proper city graph path visualization

--- src/theoric_app/theoric_v2.py
+++ src/theoric_app/theoric_v2.py
@@ -1,5 +1,7 @@
 import itertools
-
+import os
+import sys
+import time
 import numpy
 import scipy.sparse
 from datetime import datetime
@@ -18,7 +20,8 @@ import networkx as nx
 import osmnx as ox
 
 
-def render_path_as_gif(initial_graph_edges, path, img_size=1000, duration_between_steps=0.8, edge_batch_size=1):
+def render_path_as_gif(initial_graph_edges, path,
+                       img_size=1000, duration_between_steps=0.8, edge_batch_size=1):
     """
     Render final path as a gif (optional)
     """
@@ -39,7 +42,7 @@ def render_path_as_gif(initial_graph_edges, path, img_size=1000, duration_betwee
             if visit_times[edge] > 1:
                 ga.highlight_edge(*edge, color='blue')
             else:
-                ga.highlight_edge(*edge, color='green')
+                ga.highlight_edge(*edge, color='red')
 
     # for raw_edge in path:
     #     new_edge = utils.normalize_pair(*raw_edge)
@@ -50,13 +53,13 @@ def render_path_as_gif(initial_graph_edges, path, img_size=1000, duration_betwee
     #     ga.next_step()
 
     cs = edge_batch_size  # Chunk size to process paths by
-    for chunk in [path[cs*i:cs*(i+1)] for i in range(len(path) // cs + 1)]:
+    for chunk in [path[cs * i:cs * (i + 1)] for i in range(len(path) // cs + 1)]:
         for raw_edge in chunk:
             new_edge = utils.normalize_pair(*raw_edge)
             drawn_path.append(new_edge)
             visit_times[new_edge] += 1
         draw_path()
-        ga.highlight_edge(*drawn_path[-1], color='red')  # Mark last visited edge in red
+        ga.highlight_edge(*drawn_path[-1], color='yellow')  # Mark last visited edge in red
         ga.next_step()
 
     # Final result
@@ -72,16 +75,97 @@ def render_path_as_gif(initial_graph_edges, path, img_size=1000, duration_betwee
             writer.append_data(image)
 
 
-def render_osmnx_path(osmnx_graph, edge_path):
-    path = [edge[0] for edge in edge_path]
-    path.append(edge_path[0][0])
-    print(path)
-    ox.plot_graph_route(osmnx_graph, route=path, route_color='r', route_linewidth=3, orig_dest_size=30,
-                        node_size=2, save=True, filepath=f'output.png', dpi=1000)
+# def convert_osmnx_geo_coords_to_image_coords(osmnx_graph):
+#     def keep_significant(value):
+#         return int(str(int(value * 10000000))[2:])
+#
+#     node_coords = {}
+#     min_c = sys.maxsize, sys.maxsize
+#     max_c = 0, 0
+#     for node_id, data in osmnx_graph.nodes(data=True):
+#         x, y = keep_significant(data['x']), keep_significant(data['y'])
+#         node_coords[node_id] = (x, y)
+#         min_c = (min(min_c[0], x), min(min_c[1], y))
+#         max_c = (max(max_c[0], x), max(max_c[1], y))
+#         # print(node_id, '->', (x, y), (data['x'], data['y']))
+#
+#     # print('min_x =', min_c[0], 'min_y =', min_c[1])
+#     # print('max_x =', max_c[0], 'max_y =', max_c[1])
+#
+#     # diff = (max_c[0] - min_c[0], max_c[1] - min_c[1])
+#     # ratio = diff[0] / diff[1]
+#     # print('diff =', diff)
+#     # print('ratio =', ratio)
+#
+#     for node_id, coords in node_coords.items():
+#         new_coords = (coords[0] - min_c[0], coords[1] - min_c[1])
+#         new_coords = (new_coords[0] / 100, new_coords[1] / 100)
+#         node_coords[node_id] = new_coords
+#         # print(node_id, '->', new_coords)
+#
+#     return node_coords
+
+
+def render_osmnx_path(osmnx_graph, edge_path, duration_between_steps=0.5, step_size=1):
+
+    time_stamp = int(time.time())
+    output_dir = f'render_files_{time_stamp}'
+    os.makedirs(output_dir)
+
+    visit_times = {}
+    for edge in edge_path:
+        visit_times[utils.normalize_pair(*edge)] = 0
+
+    def get_edge_color(path, u, v, highlight_last=False):
+        if highlight_last and ((u, v) == path[-1] or (v, u) == path[-1]):  # Highlight last visited edge
+            return 'yellow'
+        if visit_times.get(utils.normalize_pair(u, v), 0) > 1:  # Already visited edge
+            return 'blue'
+        if (u, v) in path or (v, u) in path:  # Visited edge
+            return 'red'
+        return 'w'
+
+    def get_edge_colors(path, highlight_last=False):
+        return [get_edge_color(path, u, v, highlight_last) for (u, v) in osmnx_graph.edges()]
+
+    files = []
+
+    def render_path(path, filename, highlight_last=False):
+        edge_colors = get_edge_colors(path, highlight_last)
+        ox.plot_graph(osmnx_graph, node_color='w', node_edgecolor='k', node_size=5, node_zorder=3,
+                      edge_color=edge_colors, edge_linewidth=1,
+                      show=False, close=True, save=True, filepath=filename)
+        files.append(filename)
+
+    # # Render step by step
+    # for i in range(len(edge_path)):
+    #     current_path = edge_path[:(i + 1)]
+    #     render_path(current_path, f'{output_dir}/step_{i:05}.png', highlight_last=True)
+    #     visit_times[utils.normalize_pair(*edge_path[i])] += 1  # Update last visited pair
+
+    # Render by big steps
+    for i in range(len(edge_path) // step_size + 1):
+        start_i, end_i = step_size * i, step_size * (i + 1)
+        current_path = edge_path[:end_i]
+        render_path(current_path, f'{output_dir}/step_{i:05}.png', highlight_last=True)
+        for edge in edge_path[start_i:end_i]:
+            visit_times[utils.normalize_pair(*edge)] += 1
+
+    # Render final step
+    render_path(edge_path, f'{output_dir}/step_{len(edge_path)}.png')
+
+    print(files)
+
+    # Render final gif
+    with imageio.get_writer(f'osmnx_render_{time_stamp}.gif', mode='I', duration=duration_between_steps) as writer:
+        for file in files:
+            image = imageio.imread(file)
+            writer.append_data(image)
+
+    print(output_dir)
 
 
 def main():
-
     """Manually create a graph"""
     # edges = [  # 6 nodes
     #     (0, 1, 1),
@@ -107,6 +191,7 @@ def main():
     #  ]
     # graph_initial = GraphAdjMat(mat=mat)
 
+    # osmnx_graph = ox.graph_from_place('Arracourt, France', network_type='drive')
     osmnx_graph = ox.graph_from_place('Villejuif, France', network_type='drive')
     osmnx_graph = nx.convert_node_labels_to_integers(osmnx_graph)
     graph_initial = GraphAdjMat()
@@ -123,20 +208,17 @@ def main():
     """Compute all possible odd node pairs"""
     odd_node_pairs = list(itertools.combinations(odd_degree_nodes, 2))
     odd_node_pairs = [utils.normalize_pair(*p) for p in odd_node_pairs]
-    # print('odd_node_pairs =', odd_node_pairs)
-    print(f'{len(odd_node_pairs)} odd_node_pairs')
+    print(f'{len(odd_node_pairs)} odd node pairs')
 
     """Compute the minimum distance for every pair"""
     odd_node_pairs_shortest_distance = {}
     csr_mat = scipy.sparse.csr_matrix(graph_initial.mat)
     print('computing odd node pairs shortest paths')
     tstart = datetime.now()
-    print(tstart)
     for pair in odd_node_pairs:
         _, odd_node_pairs_shortest_distance[pair] = GraphAdjMat.shortest_path(csr_mat, *pair)
         # print(f'pair={pair} dist={dist}')
     tend = datetime.now()
-    print(tend)
     print('done, took', tend - tstart)
 
     """Create a complete graph of those pairs with the minimum distance as weight"""
@@ -149,10 +231,8 @@ def main():
     """Compute the minimum weight matching"""
     print('computing minimum weight matching')
     tstart = datetime.now()
-    print(tstart)
     min_weight_pairs = edmonds.min_weight_matching(graph_odd_complete)
     tend = datetime.now()
-    print(tend)
     print('done, took', tend - tstart)
     print('min_weight_pairs =', min_weight_pairs)
 
@@ -164,7 +244,7 @@ def main():
         pair = utils.normalize_pair(*pair)
         graph_augmented.add_edge(*pair, odd_node_pairs_shortest_distance[pair])
 
-    print(graph_augmented)
+    # print(graph_augmented)
     # graph_augmented.to_dot(filename='graph_augmented')
 
     """Get a naive path (naive because it uses the previous fakes edges)"""
@@ -184,8 +264,9 @@ def main():
 
     print(final_path)
 
-    render_path_as_gif(graph_initial.edge_list(), final_path, img_size=6880, duration_between_steps=1, edge_batch_size=10)
-    # render_osmnx_path(osmnx_graph, final_path)
+    # render_path_as_gif(graph_initial.edge_list(), final_path)
+
+    render_osmnx_path(osmnx_graph, final_path, step_size=23)
 
 
 if __name__ == '__main__':
