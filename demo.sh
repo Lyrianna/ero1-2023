3cd2417b0e3e2b5ac49fb8a0b481b9863ffafbd4
Mexane Delcroix <mexane.delcroix@epita.fr>
2021-06-04 19:17:14 +0200

    Script Montreal

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source 'venv/bin/activate'
-pip install -r requirements.txt         
-pip install python-Levenshtein 
+pip install -r requirements.txt
+pip install python-Levenshtein
 python3 ./src/practical_app/main.py "Montreal, Canada" False
