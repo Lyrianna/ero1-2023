aa87a3e9eccbdffa12411d7330032cca7a4a9a7b
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-06-04 20:58:19 +0200

    Fixed demo.sh python version (force 3.8)

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 -m venv venv/
+python3.8 -m venv venv/
 source 'venv/bin/activate'
 pip install -r requirements.txt
 pip install python-Levenshtein
-python3 ./src/practical_app/main.py "Montreal, Canada" False
+python ./src/practical_app/main.py "Montreal, Canada" False
