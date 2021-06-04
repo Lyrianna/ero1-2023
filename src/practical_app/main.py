f962e9f127e95e4ffd95cf06ab842a2ecd2af214
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-06-04 18:01:14 +0200

    Added more user info

--- src/practical_app/main.py
+++ src/practical_app/main.py
@@ -35,32 +35,40 @@ def main():
 
     sub_graphs = graph_partitionning.split_graph(graph)
     # sub_graphs = [graph]
-    # print(f'({len(sub_graphs)}) sub graphs (zones) of sizes {[len(g) for g in sub_graphs]}')
-    print(f'({len(sub_graphs)}) sub graphs (zones)')
+    print(f'({len(sub_graphs)}) sub graphs (zones) of sizes {[len(g) for g in sub_graphs]}')
+    # print(f'({len(sub_graphs)}) sub graphs (zones)')
+
+    print('\nComputing zone paths...')
+    total_t_start = datetime.now()
 
     paths = []
+    ratios = []
     for g in sub_graphs:
-        print('Computing paths...')
+        print('Computing path...')
         t_start = datetime.now()
         path, ratio = directed_solver.optimal_path(g)
         t_end = datetime.now()
-        print('Done, took in total:', t_end - t_start)
+        print('Done, took:', t_end - t_start)
         print('Path length =', len(path))
         print(f'Ratio = {ratio:.2f}% (additionally covered distance compared to initial total road length)')
         print()
         paths.append(path)
+        ratios.append(ratio)
+
+    total_t_end = datetime.now()
+    print('\nDone, in total took:', total_t_end - total_t_start)
+    avg_ratio = sum(ratios)/len(ratios)
+    print(f'Average zone ratio = {avg_ratio:.2f}%\n')
 
     with open('paths.txt', 'w') as f:
         for path in paths:
             f.write(f'{path}\n')
-    print('Final paths available in "src/paths.txt" file')
+    print('Final paths available in "src/paths.txt" file\n')
 
     if len(sys.argv) >= 3 and sys.argv[2] == 'True':
-        print()
-        print('Rendering zones')
+        print('Rendering zones\n')
         graph_rendering.render_sub_graphs(graph, sub_graphs)
-        print()
-        print('Rendering paths...')
+        print('Rendering paths...\n')
         graph_rendering.render_path_or_paths(graph, paths=paths, duration_between_steps=0.3, step_size=500, edge_width=0.1)
 
 
