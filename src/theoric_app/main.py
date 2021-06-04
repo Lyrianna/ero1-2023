6dae3c101e7aa56ebd666ac7deddf77d8108dd1f
Jeremy Croiset <jeremy.croiset@epita.fr>
2021-06-04 23:09:54 +0200

    Fixed theoric

--- src/theoric_app/main.py
+++ src/theoric_app/main.py
@@ -2,40 +2,42 @@ import graph_adj_mat
 import graph_rendering
 import solver
 
-# import osmnx as ox
-# ox.config(use_cache=True, log_console=False)
+import networkx as nx
+import osmnx as ox
+ox.config(use_cache=True, log_console=False)
 
 
 def main():
 
     """Manually create a graph"""
-    mat = [
-        [ 0,  4,  0,  0,  0,  0,  0,  8,  0],
-        [ 4,  0,  8,  0,  0,  0,  0, 11,  0],
-        [ 0,  8,  0,  7,  0,  4,  0,  0,  2],
-        [ 0,  0,  7,  0,  9,  0,  14,  0,  0],
-        [ 0,  0,  0,  9,  0, 10,  0,  0,  0],
-        [ 0,  0,  4,  0, 10,  0,  2,  0,  0],
-        [ 0,  0,  0, 14,  0,  2,  0,  1,  6],
-        [ 8, 11,  0,  0,  0,  0,  1,  0,  7],
-        [ 0,  0,  2,  0,  0,  0,  6,  7,  0]
-     ]
-    initial_graph = graph_adj_mat.GraphAdjMat(mat=mat)
+    # mat = [
+    #     [ 0,  4,  0,  0,  0,  0,  0,  8,  0],
+    #     [ 4,  0,  8,  0,  0,  0,  0, 11,  0],
+    #     [ 0,  8,  0,  7,  0,  4,  0,  0,  2],
+    #     [ 0,  0,  7,  0,  9,  0,  14,  0,  0],
+    #     [ 0,  0,  0,  9,  0, 10,  0,  0,  0],
+    #     [ 0,  0,  4,  0, 10,  0,  2,  0,  0],
+    #     [ 0,  0,  0, 14,  0,  2,  0,  1,  6],
+    #     [ 8, 11,  0,  0,  0,  0,  1,  0,  7],
+    #     [ 0,  0,  2,  0,  0,  0,  6,  7,  0]
+    #  ]
+    # initial_graph = graph_adj_mat.GraphAdjMat(mat=mat)
 
     # osmnx_graph = ox.graph_from_place('Villejuif, France', network_type='drive')
-    # osmnx_graph = nx.convert_node_labels_to_integers(osmnx_graph)
-    # graph_initial = GraphAdjMat()
-    # graph_initial.load_osmnx_graph(osmnx_graph)
+    osmnx_graph = ox.graph_from_place('Montreal, Canada', network_type='drive')
+    osmnx_graph = nx.convert_node_labels_to_integers(osmnx_graph)
+    graph_initial = graph_adj_mat.GraphAdjMat()
+    graph_initial.load_osmnx_graph(osmnx_graph)
 
-    print('graph degree =', initial_graph.degree)
-    print(initial_graph)
-    # graph_initial.to_dot(filename='initial_graph')
+    print('graph degree =', graph_initial.degree)
+    print(graph_initial)
+    # graph_initial.to_dot(filename='graph_initial')
 
-    final_path = solver.solve(initial_graph)
+    final_path = solver.solve(graph_initial)
     print('path of lenngth =', len(final_path))
     print('path =', final_path)
 
-    graph_rendering.render_path_as_gif(initial_graph.edge_list(), final_path)
+    graph_rendering.render_path_as_gif(graph_initial.edge_list(), final_path)
 
     # graph_rendering.render_osmnx_path(osmnx_graph, final_path, step_size=23)
 
